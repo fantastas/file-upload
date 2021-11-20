58834517bb66c3170d752767638b95497850dd7b
fantastas <msgitara@gmail.com>
2021-11-20 10:36:08 +0800

    urlencoded lim

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,8 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.use(json({ limit: '200mb' }));
-  app.use(urlencoded({ limit: '200mb' }));
+  // app.use(json({ limit: '200mb' }));
+  // app.use(urlencoded({ limit: '200mb' }));
   await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
