ce188cd1e51096616accf53c2224badc19faa808
fantastas <msgitara@gmail.com>
2021-11-20 10:24:17 +0800

    urlencoded limit

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
-import { json } from 'express';
+import { json, urlencoded } from 'express';
 
 dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.use(json({ limit: '200mb' }));
+  app.use(urlencoded({ limit: '200mb' }));
   await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
